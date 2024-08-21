2228fc9bea3bf0321a7005b86edd8d723f5f1696
NT-000 <nicolai.thue.reier@gmail.com>
2024-08-21 19:23:19 +0200

    Update v3

--- intro.js
+++ intro.js
@@ -3,32 +3,8 @@
 //    document.getElementById("changeBtn").innerHTML = $(showGreen(),showYellow(),showOrange());
 //}
 
-showGreen();
-function showGreen() {
-    document.getElementById('body').innerHTML = /*HTML*/`
-    <div id="body" onclick="showOrange()">
-        <div style="background-color: green;"></div> `
-;}
 
-showYellow();
-function showYellow() {
+function chColor1() {
+    document.getElementById("body").classList.toggle("changeToggle");}
 
-    document.getElementById('body').innerHTML = /*HTML*/`
-    <div id="body" onclick="showGreen()">
-        <div style="background-color: yellow;"></div> 
-`;}
 
-showOrange();
-function showOrange() {
-    document.getElementById('body').innerHTML = /*HTML*/`
-    <div id="body" onclick="showYellow()">
-        <div style="background-color: orange;"></div>
-    `;}
-
-function show(myFunction,bG1,bG2,bG3) {
-    document.getElementById('body').innerHTML = /*HTML*/`
-    <div id="body" onclick="show${myFunction}">
-    <div class="body" style="background-color: ${bG1};"></div>
-    <div class="body" style="background-color: ${bG2};"></div>
-    <div class="body" style="background-color: ${bG3};"></div>
-`;}
