8127366bf69a249aa4bb9fcad9e91241b163cf5d
NT-000 <nicolai.thue.reier@gmail.com>
2024-08-22 18:22:07 +0200

    Math.random bakgrunn

--- intro.js
+++ intro.js
@@ -3,9 +3,24 @@
 //    document.getElementById("changeBtn").innerHTML = $(showGreen(),showYellow(),showOrange());
 //}
 
+function SetRandNum() {
+    let randomNum = Math.floor(Math.random()*3);
+    console.log(randomNum)
 
-function chColor1() {
-    document.getElementById('body').classList.toggle("changeToggle");}
-   //document.getElementById('outerFrame').classList.toggle("changeToggle");}
+    if(randomNum == 1) {
+        document.body.style.backgroundColor = 'black'; 
+    }
 
+    else if(randomNum == 2) {
+        document.body.style.backgroundColor = 'yellow';
+     }
+
+     else if(randomNum == 3) {
+        document.body.style.backgroundColor = 'brown';
+     }
+
+    else {
+        document.body.style.backgroundColor = 'green';
+     }
+}
 
